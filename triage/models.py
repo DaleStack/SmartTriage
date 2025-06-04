16160d90a7853008dc1442cc55882ef07855c397
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-05 00:19:35 +0800

    created the TriageModel

--- triage/models.py
+++ triage/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class TriageModel(models.Model):
+    assigned_doctor = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                        on_delete=models.SET_NULL,
+                                        null=True,
+                                        blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    reviewed = models.BooleanField(default=False)
+
+    # Step 1: Personal Info
+    full_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10,
+                            choices=[('Male', 'Male'), 
+                                     ('Female', 'Female'), 
+                                     ('Other', 'Other')])
+    phone_number = models.CharField(max_length=15)  
+
+    # Step 2: Symptoms Assessment
+    symptoms = models.TextField()
+    duration = models.CharField(max_length=100)
+    pain_level = models.IntegerField(
+        choices=[(i, str(i)) for i in range(0, 11)] # 0 - 10 scale
+    ) 
+
+    # Step 3: Medical History
+    existing_conditions = models.TextField(blank=True)
+    allergies = models.TextField(blank=True)
+    current_medications = models.TextField(blank=True)
+    surgery = models.BooleanField(default=False)  # If patient had surgery in the past 30 days
+
+    def __str__(self):
+        return f"Triage Note - {self.full_name} ({self.submitted_at.strftime('%Y-%m-%d %H:%M')})"
