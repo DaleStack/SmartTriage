76c133f25962f08d975d0d591581630f316c30c5
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-09 00:00:44 +0800

    Patient triage view finished

--- triage/urls.py
+++ triage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import triage_view
 
 urlpatterns = [
-    path('patients/', triage_view, name='triage'),
+    path('triage/', triage_view, name='triage'),
 ]
--- triage/views.py
+++ triage/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def triage_view(request):
-    return render(request, 'triage/triage.html')
+    return render(request, 'triage/home/triage.html')
